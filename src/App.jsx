f0e4095b0bcda6d1b5e87de56f770239675996f7
Eduardo <ejrb1234@gmail.com>
2023-12-17 21:31:12 -0400

    After formatter ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-export const App = () => {
+export const TiDiListApp = () => {
   return (
     <>
       <h1 className="text-3xl font-bold text-green-500 underline">{`</App>`}</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
-import "./globals.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { TiDiListApp } from './TiDiListApp.jsx'
+import './globals.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <App />
+    <TiDiListApp />
   </React.StrictMode>
-);
+)
