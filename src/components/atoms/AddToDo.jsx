b708a89c8f8a57398b8e56918072c242d835c309
Eduardo <ejrb1234@gmail.com>
2023-12-29 20:25:01 -0400

    tooltip (component)

--- src/components/atoms/AddToDo.jsx
+++ src/components/atoms/AddToDo.jsx
@@ -1,18 +1,23 @@
 import { Plus } from 'lucide-react'
 import propTypes from 'prop-types'
+import { Tooltip } from './Tooltip/Tooltip'
 
 export const AddToDo = ({ onClick }) => {
   return (
     <button
       type="button"
       className="
-        w-11 h-11 rounded-full grid place-content-center fixed bottom-20 right-5
+        w-11 h-11 rounded-full 
+        grid place-content-center
+        fixed bottom-20 right-5
         text-gray-100
 
-        bg-teal-600 hover:bg-teal-700 "
+        bg-teal-600 hover:bg-teal-700"
       onClick={onClick}
     >
-      <Plus />
+      <Tooltip>
+        <Plus />
+      </Tooltip>
     </button>
   )
 }
--- src/components/organisms/ToDoList.jsx
+++ src/components/organisms/ToDoList.jsx
@@ -49,7 +49,6 @@ export const ToDoList = () => {
           </ToDos>
         )}
       </div>
-
     </>
   )
 }
