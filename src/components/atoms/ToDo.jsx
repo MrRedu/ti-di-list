39c7f09095489eedbcb007ace8787c5d28cbb1b7
Eduardo <ejrb1234@gmail.com>
2024-02-07 22:13:19 -0400

    added: description label + fixing styles

--- src/components/atoms/ToDo.jsx
+++ src/components/atoms/ToDo.jsx
@@ -1,14 +1,23 @@
 import propTypes from 'prop-types'
 import { InputCheckBox } from './InputCheckBox'
+import { ChevronDown } from 'lucide-react'
+import { useState } from 'react'
 
 export const ToDo = ({
   id,
   title,
-  isCompleted,
+  description,
   category,
+  isCompleted,
   handleDelete,
   handleIsCompleted,
 }) => {
+  const [showDescription, setShowDescription] = useState(false)
+
+  const handleShowDescription = () => {
+    setShowDescription(prev => !prev)
+  }
+
   return (
     <>
       <li
@@ -26,34 +35,48 @@ export const ToDo = ({
         <div
           className="
           flex gap-2    
-          w-full"
+          w-full
+          "
         >
           <InputCheckBox handleIsCompleted={handleIsCompleted} id={id} />
 
-          <label
-            htmlFor={id}
-            className="
+          <div className="w-full flex justify-between">
+            <label
+              htmlFor={id}
+              className="
               flex flex-col justify-center
-              w-full
+              w-fit
 
               line-clamp-1
               
               mr-6
               "
-          >
-            <span
-              className={`leading-none line-clamp-1
+            >
+              <span
+                className={`leading-none line-clamp-1
 
             ${isCompleted ? 'line-through' : ''}`}
-            >
-              {title}
-            </span>
-            {category && (
-              <span className="leading-none text-sm text-gray-400">{`${category}`}</span>
-            )}
-          </label>
+              >
+                {title}
+              </span>
+              {category && (
+                <span className="leading-none text-sm text-gray-400">{`${category}`}</span>
+              )}
+            </label>
+            <div className="flex justify-center items-center ">
+              <button onClick={handleShowDescription} className="p-2 ">
+                <ChevronDown
+                  className={`
+                ${showDescription ? 'rotate-180' : ''}
+                `}
+                  // transition-all duration-300
+                />
+              </button>
+            </div>
+          </div>
         </div>
       </li>
+      {showDescription && <p className="text-sm">{description}</p>}
     </>
   )
 }
@@ -61,6 +84,7 @@ export const ToDo = ({
 ToDo.propTypes = {
   id: propTypes.string.isRequired,
   title: propTypes.string.isRequired,
+  description: propTypes.string,
   category: propTypes.string,
   isCompleted: propTypes.bool.isRequired,
   handleDelete: propTypes.func.isRequired,
